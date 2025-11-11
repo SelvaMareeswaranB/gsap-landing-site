dc2af49193c60b86e9427279e9d018bdb32bb152
Selva Mareeswaran <selvamareeswaranb@gmail.com>
2025-11-11 08:56:43 +0530

    env variable bug fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import process from "node:process";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: import.meta.env.VITE_BASE_PATH || "/gsap-landing-site",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), tailwindcss()],
+    base: env.VITE_BASE_PATH || "/gsap-landing-site/",
+  };
 });
